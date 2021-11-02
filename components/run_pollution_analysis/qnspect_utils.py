cb567f0abf1e1c1443b50fc12f0e7c146dc58311
Abdul Siddiqui <abdulraheemsiddiqui@gmail.com>
2021-11-02 16:53:58 -0400

    Separate common functions

---
+++ components/run_pollution_analysis/qnspect_utils.py
@@ -0,0 +1,47 @@
+"""
+Store common functions that are required by different QNSPECT Modules
+"""
+
+
+from qgis.core import (
+    QgsProcessing,
+)
+import processing
+
+
+def perform_raster_math(
+    exprs,
+    input_dict,
+    context,
+    feedback,
+    output=QgsProcessing.TEMPORARY_OUTPUT,
+):
+    """Wrapper around QGIS GDAL Raster Calculator"""
+
+    alg_params = {
+        "BAND_A": input_dict.get("band_a", None),
+        "BAND_B": input_dict.get("band_b", None),
+        "BAND_C": input_dict.get("band_c", None),
+        "BAND_D": input_dict.get("band_d", None),
+        "BAND_E": input_dict.get("band_e", None),
+        "BAND_F": input_dict.get("band_f", None),
+        "EXTRA": "",
+        "FORMULA": exprs,
+        "INPUT_A": input_dict.get("input_a", None),
+        "INPUT_B": input_dict.get("input_b", None),
+        "INPUT_C": input_dict.get("input_c", None),
+        "INPUT_D": input_dict.get("input_d", None),
+        "INPUT_E": input_dict.get("input_e", None),
+        "INPUT_F": input_dict.get("input_f", None),
+        "NO_DATA": -9999,
+        "OPTIONS": "",
+        "RTYPE": 5,
+        "OUTPUT": output,
+    }
+    return processing.run(
+        "gdal:rastercalculator",
+        alg_params,
+        context=context,
+        feedback=feedback,
+        is_child_algorithm=True,
+    )
