ad14ea7130fb800ca672554ad3d59ce29e2e944c
Abdul Siddiqui <abdulraheemsiddiqui@gmail.com>
2021-12-01 12:02:59 -0500

    Add parsing logic

--- components/load_run/load_run.py
+++ components/load_run/load_run.py
@@ -1,11 +1,14 @@
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
-from qgis.core import QgsProcessingParameterFile
+from qgis.core import QgsProcessingParameterFile, QgsProcessingException
+
 import processing
+import json
 
 class LoadPreviousRun(QgsProcessingAlgorithm):
     load_parameters = {}
+    alg = ""
 
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterFile('RunFile', 'Run File', behavior=QgsProcessingParameterFile.File, fileFilter='QNSPECT Files (*pol.json *ero.json)', defaultValue=None))
@@ -17,11 +20,26 @@ class LoadPreviousRun(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
+        run_file = self.parameterAsString(parameters, "RunFile", context)
+        if run_file.lower().endswith(".pol.json"):
+            self.alg = "script:Run Pollution Analysis"
+        elif run_file.lower().endswith(".ero.json"):
+            self.alg = "script:Run Erosion Analysis" 
+        else:
+            raise QgsProcessingException("Wrong or missing parameter value: Run File")
+            
+        with open(run_file) as f:
+            data = json.load(f)
+
+        self.load_parameters = data["Inputs"]
+        
         return results
 
     def postProcessAlgorithm(self, context, feedback):
-        
-        processing.execAlgorithmDialog("native:dropgeometries", self.load_parameters)
+        # this can be handled better through QGIS Tasks
+        # https://www.opengis.ch/2016/09/07/using-threads-in-qgis-python-plugins/
+        # https://www.opengis.ch/2018/06/22/threads-in-pyqgis3/
+        processing.execAlgorithmDialog(self.alg, self.load_parameters)
         return {}
 
 
