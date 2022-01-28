69b50784ac0b9e640e00aa8b28d06bc4e5578f18
Ian Todd <itodd@dewberry.com>
2022-01-28 12:35:59 -0500

    Merge with development

---
+++ algorithms/run_analysis/analysis_utils.py
@@ -0,0 +1,99 @@
+"""
+Store common functions that are required by different analysis modules
+"""
+
+
+from qgis.core import (
+    QgsProcessing,
+    QgsVectorLayer,
+    Qgis,
+    QgsRasterLayer,
+    QgsProcessingException,
+)
+import processing
+import os
+from pathlib import Path
+
+LAND_USE_TABLES = {1: "C-CAP", 2: "NLCD"}
+LAND_USE_PATH = (
+    f"file:///{Path(__file__).parent.parent.parent / 'resources' / 'coefficients'}"
+)
+
+
+def convert_raster_data_type_to_float(
+    raster_layer: QgsRasterLayer, context, feedback, outputs, output=None,
+):
+    """Converts the input raster layer's data type from an integer to a float 32.
+    If the input raster's data type is already a float, it will return the input raster layer."""
+    if output is None:
+        output = QgsProcessing.TEMPORARY_OUTPUT
+    data_type = raster_layer.dataProvider().dataType(1)
+    if data_type not in [Qgis.Float32, Qgis.Float64, Qgis.CFloat32, Qgis.CFloat64]:
+        alg_params = {
+            "BANDS": [1],
+            "DATA_TYPE": 6,  # Float 32
+            "INPUT": raster_layer,
+            "OPTIONS": "",
+            "OUTPUT": output,
+        }
+        outputs["RearrangeBands"] = processing.run(
+            "gdal:rearrange_bands",
+            alg_params,
+            context=context,
+            feedback=feedback,
+            is_child_algorithm=True,
+        )
+        return outputs["RearrangeBands"]["OUTPUT"]
+    return raster_layer
+
+
+def reclassify_land_use_raster_by_table_field(
+    lu_raster: str,
+    lookup_layer: QgsVectorLayer,
+    value_field: str,
+    context,
+    feedback,
+    output=None,
+):
+    """Wrapper around QGIS Reclassify by Layer"""
+    if output is None:
+        output = QgsProcessing.TEMPORARY_OUTPUT
+
+    alg_params = {
+        "DATA_TYPE": 5,
+        "INPUT_RASTER": lu_raster,
+        "INPUT_TABLE": lookup_layer,
+        "MAX_FIELD": "lu_value",
+        "MIN_FIELD": "lu_value",
+        "NODATA_FOR_MISSING": True,
+        "NO_DATA": -9999,
+        "RANGE_BOUNDARIES": 2,
+        "RASTER_BAND": 1,
+        "VALUE_FIELD": value_field,
+        "OUTPUT": output,
+    }
+    return processing.run(
+        "native:reclassifybylayer",
+        alg_params,
+        context=context,
+        feedback=feedback,
+        is_child_algorithm=True,
+    )
+
+
+def extract_lookup_table(alg, parameters, context):
+    """Extract the lookup table as a vector layer. Retuns None if the selection combination was invalid"""
+    if parameters["LookupTable"]:
+        return alg.parameterAsVectorLayer(parameters, "LookupTable", context)
+
+    land_use_type = alg.parameterAsEnum(parameters, "LandUseType", context)
+    if land_use_type > 0:
+        return QgsVectorLayer(
+            os.path.join(LAND_USE_PATH, f"{LAND_USE_TABLES[land_use_type]}.csv"),
+            "Land Use Lookup Table",
+            "delimitedtext",
+        )
+    else:
+        raise QgsProcessingException(
+            "Land Use Lookup Table must be provided with Custom Land Use Type.\n"
+        )
--- algorithms/run_analysis/run_pollution_analysis.py
+++ algorithms/run_analysis/run_pollution_analysis.py
@@ -11,7 +11,6 @@
  ***************************************************************************/
 """
 
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
 __author__ = "Abdul Raheem Siddiqui"
 __date__ = "2021-12-29"
 __copyright__ = "(C) 2021 by NOAA"
@@ -19,15 +18,6 @@ __copyright__ = "(C) 2021 by NOAA"
 # This will get replaced with a git SHA1 when you do a git archive
 
 __revision__ = "$Format:%H$"
-=======
-__author__ = 'Abdul Raheem Siddiqui'
-__date__ = '2021-12-29'
-__copyright__ = '(C) 2021 by NOAA'
-
-# This will get replaced with a git SHA1 when you do a git archive
-
-__revision__ = '$Format:%H$'
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
 from qgis.core import (
     QgsProcessing,
@@ -65,7 +55,6 @@ sys.path.append(os.path.dirname(cmd_folder))
 from Curve_Number import Curve_Number
 from Runoff_Volume import Runoff_Volume
 from qnspect_utils import perform_raster_math, grass_material_transport, filter_matrix
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
 from analysis_utils import (
     extract_lookup_table,
     reclassify_land_use_raster_by_table_field,
@@ -100,39 +89,6 @@ from QNSPECT.qnspect_algorithm import QNSPECTAlgorithm
 
 class RunPollutionAnalysis(QNSPECTAlgorithm):
     #     grouper = None
-=======
-
-from QNSPECT.qnspect_algorithm import QNSPECTAlgorithm
-
-
-# class LayerGrouper(QgsProcessingLayerPostProcessorInterface):
-#     project = None
-#     group = None
-
-#     def __init__(self, group_name):
-#         self.group_name = group_name
-#         super().__init__()
-
-#     def postProcessLayer(self, layer, context, feedback):
-#         if not self.project:
-#             self.project = context.project()
-#         if not self.group:
-#             root = self.project.instance().layerTreeRoot()
-#             self.group = root.addGroup(self.group_name)
-#         self.group.addLayer(layer)
-#         return {}
-
-
-# class LayerPostProcessor(QgsProcessingLayerPostProcessorInterface):
-#     def postProcessLayer (self, layer, context, feedback):
-#         if layer.isValid():
-#             layer.loadNamedStyle('Runoff Local.qml')
-
-class RunPollutionAnalysis(QNSPECTAlgorithm):
-    lookup_tables = {1: "C-CAP", 2: "NLCD"}
-    default_lookup_path = f"file:///{Path(__file__).parent.parent.parent / 'resources' / 'coefficients'}"
-#     grouper = None
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
     def initAlgorithm(self, config=None):
         self.addParameter(
@@ -295,24 +251,7 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
         precip_raster = self.parameterAsRasterLayer(parameters, "PrecipRaster", context)
 
         ## Extract Lookup Table
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
         lookup_layer = extract_lookup_table(self, parameters, context)
-=======
-        if parameters["LookupTable"]:
-            lookup_layer = self.parameterAsVectorLayer(
-                parameters, "LookupTable", context
-            )
-        elif land_use_type > 0:  # create lookup table from default
-            lookup_layer = QgsVectorLayer(
-                f"{self.default_lookup_path}/{self.lookup_tables[land_use_type]}.csv",
-                "Land Use Lookup Table",
-                "delimitedtext",
-            )
-        else:
-            raise QgsProcessingException(
-                "Land Use Lookup Table must be provided with Custom Land Use Type.\n"
-            )
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         # handle different cases in input matrix and lookup layer
         lookup_fields = {f.name().lower(): f.name() for f in lookup_layer.fields()}
@@ -326,11 +265,7 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
             raise QgsProcessingException(
                 "One or more of the Pollutants is not a column in the Land Use Lookup Table. Either remove the pollutants from Desired Outputs or provide a custom lookup table with desired pollutants.\n"
                 + f"Missing Pollutants:\n{[pol.lower() for pol in desired_pollutants if not pol.lower() in lookup_fields.keys()]}\n"
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
             )
-=======
-            )    
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         # assert all Raster CRS are same and Raster Pixel Units too
 
@@ -369,18 +304,11 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
             outputs["Runoff Local"] = runoff_vol.calculate_Q(runoff_output)
             results["Runoff Local"] = outputs["Runoff Local"]["OUTPUT"]
             if load_outputs:
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
                 self.handle_post_processing(
                     outputs["Runoff Local"]["OUTPUT"], "Runoff Local (L)", context
                 )
         else:
             outputs["Runoff Local"] = runoff_vol.calculate_Q()
-=======
-                self.handle_post_processing(outputs["Runoff Local"]["OUTPUT"], "Runoff Local (L)", context)
-        else:
-            outputs["Runoff Local"] = runoff_vol.calculate_Q()
-
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         ## Pollutant rasters
         for pol in desired_pollutants:
@@ -408,18 +336,13 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
             )
             results[pol + " Local"] = outputs[pol + " Local"]["OUTPUT"]
             if load_outputs:
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
                 self.handle_post_processing(
                     outputs[pol + " Local"]["OUTPUT"], f"{pol} Local (mg)", context
                 )
-=======
-                self.handle_post_processing(outputs[pol + " Local"]["OUTPUT"], f"{pol} Local (mg)", context)
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         # Accumulated Runoff Calculation (L)
         if "runoff" in [out.lower() for out in desired_outputs]:
             runoff_output = os.path.join(run_out_dir, f"Runoff Accumulated.tif")
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
             # see this issue for discussion on nodat value issue https://github.com/Dewberry/QNSPECT/issues/29
             outputs["Runoff Accumulated"] = grass_material_transport(
                 parameters["ElevatoinRaster"],
@@ -446,20 +369,10 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                 feedback,
                 mfd,
             )
-=======
-            #see this issue for discussion on nodat value issue https://github.com/Dewberry/QNSPECT/issues/29
-            outputs["Runoff Accumulated"] = grass_material_transport(parameters["ElevatoinRaster"], outputs["Runoff Local"]["OUTPUT"], context, feedback, mfd, runoff_output)
-            results["Runoff Accumulated"] = outputs["Runoff Accumulated"]["accumulation"]
-            if load_outputs:
-                self.handle_post_processing(outputs["Runoff Accumulated"]["accumulation"], "Runoff Accumulated (L)", context)
-        else:
-            outputs["Runoff Accumulated"] = grass_material_transport(parameters["ElevatoinRaster"], outputs["Runoff Local"]["OUTPUT"], context, feedback, mfd)
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         # Accumulated Pollutants
         for pol in desired_pollutants:
             # Accumulated Pollutant (mg)
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
             outputs[pol + "accum_mg"] = grass_material_transport(
                 parameters["ElevatoinRaster"],
                 outputs[pol + " Local"]["OUTPUT"],
@@ -468,10 +381,6 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                 mfd,
             )
 
-=======
-            outputs[pol + "accum_mg"] = grass_material_transport(parameters["ElevatoinRaster"], outputs[pol + " Local"]["OUTPUT"], context, feedback, mfd)
-            
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
             # convert to kg
             input_params = {
                 "input_a": outputs[pol + "accum_mg"]["accumulation"],
@@ -483,7 +392,6 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                 context,
                 feedback,
                 os.path.join(run_out_dir, f"{pol} Accumulated.tif"),
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
             )
             results[pol + " Accumulated"] = outputs[pol + " Accumulated"]["OUTPUT"]
             if load_outputs:
@@ -492,12 +400,6 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                     f"{pol} Accumulated (kg)",
                     context,
                 )
-=======
-            )            
-            results[pol + " Accumulated"] = outputs[pol + " Accumulated"]["OUTPUT"]
-            if load_outputs:
-                self.handle_post_processing(outputs[pol + " Accumulated"]["OUTPUT"], f"{pol} Accumulated (kg)", context)
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         # Concentration Calculations
         if conc_out:
@@ -507,11 +409,7 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                     "input_a": outputs[pol + "accum_mg"]["accumulation"],
                     "band_a": "1",
                     "input_b": outputs["Runoff Accumulated"]["accumulation"],
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
                     "band_b": "1",
-=======
-                    "band_b": "1",                   
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
                 }
                 outputs[pol + " Concentration"] = perform_raster_math(
                     "(A / B)",
@@ -519,7 +417,6 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                     context,
                     feedback,
                     os.path.join(run_out_dir, f"{pol} Concentration.tif"),
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
                 )
                 results[pol + " Concentration"] = outputs[pol + " Concentration"][
                     "OUTPUT"
@@ -530,12 +427,6 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
                         f"{pol} Concentration (mg/L)",
                         context,
                     )
-=======
-                )            
-                results[pol + " Concentration"] = outputs[pol + " Concentration"]["OUTPUT"]
-                if load_outputs:
-                    self.handle_post_processing(outputs[pol + " Concentration"]["OUTPUT"], f"{pol} Concentration (mg/L)", context)
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 
         # Configuration file
         run_dict["Inputs"] = parameters
@@ -571,7 +462,6 @@ class RunPollutionAnalysis(QNSPECTAlgorithm):
     def shortHelpString(self):
         return """<html><body>
 <a href="https://www.noaa.gov/">Documentation</a>
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
 <h2>Algorithm Description</h2>
 <p>The `Run Pollution Analysis` algorithm estimates annual runoff volume and pollutant loading for a given area on per cell and accumulated bases. The Runoff Volume is calculated using the NRCS Curve Number method, while pollution loading is calculated using Land Cover as a proxy.</p>
 The user must provide Elevation, Land Use, Soil, and Precipitation rasters for the area of interest. The user is also optionally required to provide a lookup table that relates different land use classes in the provided Land Use raster with Curve Number and pollutant loading.
@@ -616,94 +506,6 @@ To exclude an output from the analysis, write N in the Output column. You must c
 <h2>Outputs</h2>
 <h3>Folder for Run Outputs</h3>
 <p>The algorithm outputs and configuration file will be saved in this directory in a separate folder.</p>
-=======
-
-<h2>Algorithm Description</h2>
-
-<p>The `Run Pollution Analysis` algorithm estimates annual runoff volume and pollutant loading for a given area on per cell and accumulated bases. The Runoff Volume is calculated using the NRCS Curve Number method, while pollution loading is calculated using Land Cover as a proxy.</p>
-
-The user must provide Elevation, Land Use, Soil, and Precipitation rasters for the area of interest. The user is also optionally required to provide a lookup table that relates different land use classes in the provided Land Use raster with Curve Number and pollutant loading.
-
-This analysis should be performed on a watershed level to account for all upstream flow at a cell. For accurate results, the area of interest should fully envelop the watershed in consideration.
-
-GRASS `r.watershed`function is used by the algorithm under the hood to calculate runoff and accumulation.</p>
-
-<h2>Input Parameters</h2>
-
-<h3>Run Name</h3>
-
-<p>Name of the run. The algorithm will create a folder with this name and save all outputs and a configuration file in that folder.</p>
-
-<h3>Elevation Raster</h3>
-
-<p>Elevation raster for the area of interest. This can be in any elevation unit. The algorithm only uses elevation data to calculate flow direction and flow accumulation throughout a watershed. </p>
-
-<h3>Soil Raster</h3>
-
-<p>Hydrologic Soil Group raster for the area of interest with following mapping <code>{'A': 1, 'B': 2, 'C': 3, 'D':4, 'A/D':5, 'B/D':6, 'C/D':7, 'W':8, Null: 9}</code>. The soil raster is used to generate runoff estimates using NRCS Curve Number method.</p>
-
-<h3>Precipitation Raster</h3>
-
-<p>Annual precipitation amounts in inches or millimeters for the area of interest. The precipitation values are used to calculate access runoff.</p>
-
-<h3>Precipitation Raster Units</h3>
-
-<p>Units of the precipitation raster, inches or millimeters.</p>
-
-<h3>Number of Rainy Days in a Year</h3>
-
-<p>This field indicates the average number of days rain occurs in one year in the area of interest. A rainy day is a day on which there was enough rain to produce runoff. The higher number of rainy days reduces access runoff volume by increasing retention.</p>
-
-<h3>Land Use Raster</h3>
-
-<p>Land Cover/Classification raster for the area of interest. The algorithm uses Land Use Raster and Lookup Table to determine each cell's runoff and pollution potential.</p>
-
-<h3>Land Use Type</h3>
-
-<p>Type of Land Use raster. If the Land Use raster is not of type C-CAP or NCLD, select custom for this field and supply a lookup table in the `Land Use Lookup Table` field.</p>
-
-<h3>Land Use Lookup Table [optional]</h3>
-
-<p>Lookup table to relate each land use class with Curve Number and pollutant load. The user can skip providing a lookup table if the land use
-type is not custom; the algorithm will utilize the default lookup table for the land use type selected in the previous option.
-
-The table must contain all land use classes available in the land use raster and all pollutants that have Output = Y in the `Desired Outputs` parameter.</p>
-
-<h3>Desired Outputs</h3>
-
-<p>In addition to the runoff, the algorithm will output the following rasters for each pollutant added here with Output column as Y:
-
-- Local (per cell) pollutant load [mg]
-- Accumulated (all upstream cell) pollutant load [kg]
-- Concentration (accumulated pollutant mass divided by accumulated runoff volume) [mg/L]. The concentration raster will only be outputted if the Output Concentration Raster option is checked in Advanced Parameters
-
-The user can add more pollutants here as long as the lookup coefficients for each pollutant are supplied in the land use lookup table.
-
-To exclude an output from the analysis, write N in the Output column. You must click Ok after editing to save your changes.</p>
-
-<h2>Advanced Parameters</h2>
-
-<h3>Output Concentration Raster</h3>
-
-<p>The concentration raster will only be outputted if the Output Concentration Raster option is checked in Advanced Parameters. Default is unchecked.</p>
-
-<h3>Use Multi Flow Direction [MFD] Routing</h3>
-
-<p>By default, the Single Flow Direction [SFD] option is used for flow routing. Multi Flow Direction [MFD] routing will be utilized for the whole analysis if this option is checked. The algorithm passes these flags to GRASS `r.watershed` function, which is the computational engine for runoff direction and accumulation calculations.</p>
-
-<h3>Treat Dual Category Soils as</h3>
-
-<p>Certain areas can have dual soil types (A/D, B/D, or C/D). These areas possess characteristics of Hydrologic Soil Group D during undrained conditions and characterstics of Hydrologic Soil Group A/B/C for drained conditions.</p>
-
-<p>In this parameter, user can specify if these areas should be treated as drained, undrained, or average of both conditions. If the average option is selected, the algorithm will use the average of drained and undrained Curve Number for runoff estimations.</p>
-
-<h2>Outputs</h2>
-
-<h3>Folder for Run Outputs</h3>
-
-<p>The algorithm outputs and configuration file will be saved in this directory in a separate folder.</p>
-
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
 </body></html>"""
 
     def createInstance(self):
@@ -714,22 +516,8 @@ To exclude an output from the analysis, write N in the Output column. You must c
         layer_details = context.LayerDetails(
             display_name, context.project(), display_name
         )
-<<<<<<< HEAD:components/run_analysis/run_pollution_analysis.py
         # layer_details.setPostProcessor(self.grouper)
         context.addLayerToLoadOnCompletion(
             layer, layer_details,
         )
 
-=======
-
-    def handle_post_processing(self, layer, display_name, context):
-        
-        layer_details = context.LayerDetails(
-                display_name, context.project(), display_name
-            )
-        # layer_details.setPostProcessor(self.grouper)
-        context.addLayerToLoadOnCompletion(
-            layer,
-            layer_details,
-        )       
->>>>>>> origin/development:algorithms/run_analysis/run_pollution_analysis.py
